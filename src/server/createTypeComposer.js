42e4880018f062bac7992f87a6a22303e14464dd
Art Nattapat <nattapat.art@gmail.com>
2017-03-08 18:59:31 +0700

    add filterArgs but not complete yet

--- src/server/createTypeComposer.js
+++ src/server/createTypeComposer.js
@@ -8,6 +8,19 @@ function createTypeComposer(options = {}) {
   let model = options.model || mongoose.model('Comment', commentSchema);
   const typeComposer = composeWithMongoose(model);
 
+  const extendedResolver = typeComposer
+    .getResolver('findMany')
+    .addFilterArg({
+      name: 'atSlug',
+      type: 'String',
+      query: (query, value, resolveParams) => {
+        query.slug = new RegExp(`^((${value})((?!/).*))$`, 'i');
+      },
+    });
+  
+  extendedResolver.name = 'findMany';
+  typeComposer.addResolver(extendedResolver);
+
   Object.keys(resolvers).forEach(key => {
     const createResolver = resolvers[key];
     typeComposer.addResolver(createResolver({ model, typeComposer }));
