6573f134128f99c5dd4120f9c69220d55bf2cc9e
Art Nattapat <nattapat.art@gmail.com>
2017-03-13 19:56:21 +0700

    add createReducerOnReply

--- src/react-native/index.js
+++ src/react-native/index.js
@@ -4,3 +4,11 @@
 
 import CommentListView from './containers/CommentListView';
 export * from './containers/CommentListView';
+
+export const createReducerOnReply = (discussionRef, updateQuery) => (previousResult, action, variables) => {
+  if (action.type === 'APOLLO_MUTATION_RESULT' && action.operationName === 'Reply' && action.variables && action.variables.discussionRef === discussionRef) {
+    const reply = action.result.data.reply;
+    return updateQuery(previousResult, reply);
+  }
+  return previousResult;
+}
